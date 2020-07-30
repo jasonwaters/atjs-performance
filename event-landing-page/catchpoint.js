4c72dbdfe80f659d1d1e0ce96ac39ccc5a6af9b0
Jason Waters <waters@adobe.com>
2020-07-30 14:29:25 -0700

    add tracepoints

--- event-landing-page/catchpoint.js
+++ event-landing-page/catchpoint.js
@@ -1,7 +1,12 @@
 open("https://target-perf-site.netlify.app/?ondevice=true")
 
 waitForNoRequest(5000)
-runScript("var atLibraryLoaded = perfStats['atLibraryLoaded'];var artifactDownloaded = perfStats['artifactDownloaded'];var decisioningFinished = perfStats['decisioningFinished'];var requestCompleted = perfStats['requestCompleted'];var offersRendered = perfStats['offersRendered'];var endToEndOffersRendered = perfStats['endToEndOffersRendered'];")
+runScript("var atLibraryLoaded = perfStats['atLibraryLoaded'];var artifactDownloaded = perfStats['artifactDownloaded'];var decisioningFinished = perfStats['decisioningFinished'];var requestCompleted = perfStats['requestCompleted'];var offersRendered = perfStats['offersRendered'];var endToEndOffersRendered = perfStats['endToEndOffersRendered'];var decisioningMethod = perfStats['decisioningMethod'];var property = perfStats['property'];var visit = perfStats['visit'];")
+
+
+storeScriptVariable("decisioningMethod","decisioningMethod;")
+storeScriptVariable("property","property;")
+storeScriptVariable("visit","visit;")
 storeScriptVariable("atLibraryLoaded","atLibraryLoaded;")
 storeScriptVariable("artifactDownloaded","artifactDownloaded;")
 storeScriptVariable("decisioningFinished","decisioningFinished;")
@@ -9,6 +14,9 @@ storeScriptVariable("requestCompleted","requestCompleted;")
 storeScriptVariable("offersRendered","offersRendered;")
 storeScriptVariable("endToEndOffersRendered","endToEndOffersRendered;")
 
+setTracepoint("t12125","${var(decisioningMethod)}")
+setTracepoint("t65215","${var(property)}")
+setTracepoint("t36912","${var(visit)}")
 setIndicator("i54417","${var(atLibraryLoaded)}")
 setIndicator("i21234","${var(artifactDownloaded)}")
 setIndicator("i41521","${var(decisioningFinished)}")
@@ -18,7 +26,11 @@ setIndicator("i33428","${var(endToEndOffersRendered)}")
 
 open("https://target-perf-site.netlify.app/?ondevice=true&repeatvisit=true")
 waitForNoRequest(5000)
-runScript("var atLibraryLoaded = perfStats['atLibraryLoaded'];var artifactDownloaded = perfStats['artifactDownloaded'];var decisioningFinished = perfStats['decisioningFinished'];var requestCompleted = perfStats['requestCompleted'];var offersRendered = perfStats['offersRendered'];var endToEndOffersRendered = perfStats['endToEndOffersRendered'];")
+runScript("var atLibraryLoaded = perfStats['atLibraryLoaded'];var artifactDownloaded = perfStats['artifactDownloaded'];var decisioningFinished = perfStats['decisioningFinished'];var requestCompleted = perfStats['requestCompleted'];var offersRendered = perfStats['offersRendered'];var endToEndOffersRendered = perfStats['endToEndOffersRendered'];var decisioningMethod = perfStats['decisioningMethod'];var property = perfStats['property'];var visit = perfStats['visit'];")
+
+storeScriptVariable("decisioningMethod","decisioningMethod;")
+storeScriptVariable("property","property;")
+storeScriptVariable("visit","visit;")
 storeScriptVariable("atLibraryLoaded","atLibraryLoaded;")
 storeScriptVariable("artifactDownloaded","artifactDownloaded;")
 storeScriptVariable("decisioningFinished","decisioningFinished;")
@@ -26,12 +38,12 @@ storeScriptVariable("requestCompleted","requestCompleted;")
 storeScriptVariable("offersRendered","offersRendered;")
 storeScriptVariable("endToEndOffersRendered","endToEndOffersRendered;")
 
+setTracepoint("t12125","${var(decisioningMethod)}")
+setTracepoint("t65215","${var(property)}")
+setTracepoint("t36912","${var(visit)}")
 setIndicator("i54417","${var(atLibraryLoaded)}")
 setIndicator("i21234","${var(artifactDownloaded)}")
 setIndicator("i41521","${var(decisioningFinished)}")
 setIndicator("i38844","${var(requestCompleted)}")
 setIndicator("i64216","${var(offersRendered)}")
 setIndicator("i33428","${var(endToEndOffersRendered)}")
-
-
-// setTracepoint()
