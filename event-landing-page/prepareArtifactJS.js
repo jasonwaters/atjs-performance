e0f14dfd5bffc0f867d5ddf91a615cf667eac5ca
Jason Waters <waters@adobe.com>
2020-08-25 12:03:55 -0700

    real world test bundle

--- event-landing-page/prepareArtifactJS.js
+++ event-landing-page/prepareArtifactJS.js
@@ -2,7 +2,8 @@ const fs = require("fs");
 
 var at_property = {
 	"lean": "be92ac4c-e72f-9f82-2a80-2c211ea86578",
-	"heavy": "693de2cd-ac92-d2c7-59fc-a3c0f2bce646"
+	"heavy": "693de2cd-ac92-d2c7-59fc-a3c0f2bce646",
+	"real": "bf77db72-3ab6-64ce-3bc5-6208be2f09c1"
 }
 
 function escapeSpecial(str) {
